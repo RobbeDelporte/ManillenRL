b21d04ccd7f17b99ad12911a3e04679cf3583600
unknown <robbe.delporte@ugent.be>
2022-07-15 01:11:45 +0200

    a

--- ManillenRL/manillenRL.py
+++ ManillenRL/manillenRL.py
@@ -3,13 +3,13 @@ from ManillenAgents import *
 
 
 
-learningIterations = 0
+learningIterations = 40000
 
 evaluationIterations = 20000
 
-p1 = SmartAgentV2()
-p3 = SmartAgentV2()
+p1 = LearningAgent()
 p2 = SmartAgentV2()
+p3 = LearningAgent()
 p4 = SmartAgentV2()
 
 players1 = [p1,p2,p3,p4]
@@ -45,8 +45,8 @@ for i in range(learningIterations):
 
 print("LEARNING DONE")
 
-team1CumulativeReward = 0
-team2CumulativeReward = 0
+team1CumulativePoints = 0
+team2CumulativePoints = 0
 
 for i in range(evaluationIterations):
     troef = i%4 
@@ -67,12 +67,12 @@ for i in range(evaluationIterations):
             team1Reward, team2Reward = me.getRewards()
         else:
             team2Reward, team1Reward = me.getRewards()
-        team1CumulativeReward += team1Reward
-        team2CumulativeReward += team2Reward
+        team1CumulativePoints += team1Reward
+        team2CumulativePoints += team2Reward
 
     if i%5000 == 0:
         print(f"evaluating: {i}/{evaluationIterations}")
 
 print("ALL ITERATIONS DONE")
-print(f"Final Score: {team1CumulativeReward} | {team2CumulativeReward}")
-print(f'Score percentile: {team1CumulativeReward/(team1CumulativeReward+team2CumulativeReward)*100}%')
+print(f"Final Score: {team1CumulativePoints} | {team2CumulativePoints}")
+print(f'Score percentile: {team1CumulativePoints/(team1CumulativePoints+team2CumulativePoints)*100}%')
